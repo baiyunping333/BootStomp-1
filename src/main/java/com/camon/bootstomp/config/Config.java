2882942ce4e2328c7d7b7e3a0df70d3250db80f0
camon <camon85@gmail.com>
2016-02-14 01:24:13 +0900

    ajp 포트 변경

--- src/main/java/com/camon/bootstomp/config/Config.java
+++ src/main/java/com/camon/bootstomp/config/Config.java
@@ -17,7 +17,7 @@ public class Config {
         TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
         Connector ajpConnector = new Connector("AJP/1.3");
         ajpConnector.setProtocol("AJP/1.3");
-        ajpConnector.setPort(8009);
+        ajpConnector.setPort(8010);
         ajpConnector.setSecure(false);
         ajpConnector.setAllowTrace(false);
         ajpConnector.setScheme("http");
