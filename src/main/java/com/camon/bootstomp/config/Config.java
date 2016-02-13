630d6d34c681fc5ae51ae752d88b1b6055e8f357
camon <camon85@gmail.com>
2016-02-14 01:12:24 +0900

    ajp 설정

---
+++ src/main/java/com/camon/bootstomp/config/Config.java
@@ -0,0 +1,27 @@
+package com.camon.bootstomp.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by jooyong on 2015-11-13.
+ */
+@Configuration
+public class Config {
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        Connector ajpConnector = new Connector("AJP/1.3");
+        ajpConnector.setProtocol("AJP/1.3");
+        ajpConnector.setPort(8009);
+        ajpConnector.setSecure(false);
+        ajpConnector.setAllowTrace(false);
+        ajpConnector.setScheme("http");
+        tomcat.addAdditionalTomcatConnectors(ajpConnector);
+        return tomcat;
+    }
+}
